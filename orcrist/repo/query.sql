2e0728707c3c693b358de5a791695149fded80c1
vegris <1cheaterok1@gmail.com>
2025-03-18 10:52:40 +0300

    Add repo package
    Define queries and use sqlc to generate code

---
+++ orcrist/repo/query.sql
@@ -0,0 +1,27 @@
+-- name: GetAliveSession :one
+SELECT * FROM sessions
+WHERE session_id = $1 AND ends_at < CURRENT_TIMESTAMP;
+
+-- name: RefreshSession :one
+UPDATE sessions
+SET ends_at = CURRENT_TIMESTAMP + MAKE_INTERVAL(secs => @session_duration::integer), updated_at = CURRENT_TIMESTAMP
+WHERE session_id = $1
+RETURNING *;
+
+-- name: GetDeviceByExternalDeviceID :one
+SELECT * from devices
+WHERE external_device_id = $1;
+
+-- name: CreateDevice :one
+INSERT INTO devices (device_id, source, external_device_id, metadata)
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: CreateSession :one
+INSERT INTO sessions (session_id, device_id, ends_at)
+VALUES ($1, $2, CURRENT_TIMESTAMP + MAKE_INTERVAL(secs => @session_duration::integer))
+RETURNING *;
+
+-- name: RemoveStaleSessions :exec
+DELETE FROM sessions
+WHERE ends_at <= CURRENT_TIMESTAMP - MAKE_INTERVAL(days => @days);
