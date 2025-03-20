72a2bb1ab12dc528cdb9ad01bc5270481e427cbc
vegris <1cheaterok1@gmail.com>
2025-03-20 10:44:12 +0300

    Add GetTokenHandler without future tokens

--- orcrist/handlers/get_token_handler.go
+++ orcrist/handlers/get_token_handler.go
@@ -1,6 +1,117 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/vegris/alas-go/orcrist/config"
+	"github.com/vegris/alas-go/orcrist/events"
+	"github.com/vegris/alas-go/orcrist/sessions"
+	"github.com/vegris/alas-go/shared/token"
+)
+
+type tokenRequestResponse struct {
+	Status   string `json:"status"`
+	Token    string `json:"token"`
+	TokenTTL int64  `json:"ttl"`
+}
+
+type errResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+var (
+	errBadToken     = errors.New("Orc token is invalid")
+	errReadError    = errors.New("Failed to read request")
+	errRequestError = errors.New("Request is malformed")
+)
 
 func HandleGetToken(w http.ResponseWriter, r *http.Request) {
+	tokenBinary := r.Header.Get("x-goblin")
+
+	oldToken, err := getOldToken(tokenBinary)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+
+	// Return the same token if it's fresh enough
+	if oldToken != nil && !isTokenExpired(oldToken) {
+		json.NewEncoder(w).Encode(tokenRequestResponse{Status: "OK", Token: tokenBinary, TokenTTL: oldToken.ExpireAt})
+		return
+	}
+
+	body, err := readBody(r)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+
+	request, err := parseRequest(body)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+
+	var token *token.Token
+	if oldToken != nil {
+		token = sessions.CreateToken(request)
+	} else {
+		token = sessions.RefreshToken(request, oldToken)
+	}
+
+	tokenBinary, err = token.Encode(config.Config.TokenSecret)
+	if err != nil {
+		log.Fatalf("Error encoding token: %v", tokenBinary)
+	}
+
+	// TODO: produce keep alive tokens
+
+	json.NewEncoder(w).Encode(tokenRequestResponse{Status: "OK", Token: tokenBinary, TokenTTL: token.ExpireAt})
+}
+
+func getOldToken(tokenBinary string) (*token.Token, error) {
+	if tokenBinary == "" {
+		return nil, nil
+	}
+
+	t, err := token.Decode(tokenBinary, config.Config.TokenSecret)
+	if err != nil {
+		return nil, errBadToken
+	}
+
+	return t, nil
+}
+
+func isTokenExpired(token *token.Token) bool {
+	return token.ExpireAt <= time.Now().Unix()
+}
+
+func readBody(r *http.Request) ([]byte, error) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, errReadError
+	}
+	defer r.Body.Close()
+	return body, nil
+}
+
+func parseRequest(body []byte) (*events.GetTokenRequest, error) {
+	request, err := events.ParseGetTokenRequest(body)
+	if err != nil {
+		return nil, errRequestError
+	}
+	return request, nil
+}
+
+func handleError(w http.ResponseWriter, err error) {
+	json.NewEncoder(w).Encode(errResponse{
+		Status:  "ERROR",
+		Message: err.Error(),
+	})
 }
---
+++ orcrist/sessions/sessions.go
@@ -0,0 +1,137 @@
+package sessions
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/vegris/alas-go/orcrist/app"
+	"github.com/vegris/alas-go/orcrist/events"
+	"github.com/vegris/alas-go/orcrist/repo"
+	"github.com/vegris/alas-go/shared/token"
+)
+
+const sessionDuration = time.Minute
+
+var sessionDurationSecs = int32(sessionDuration.Seconds())
+
+func RefreshToken(request *events.GetTokenRequest, t *token.Token) *token.Token {
+	ctx := context.Background()
+
+	session := refreshExistingSession(ctx, request.SessionID)
+	if session == nil {
+		session = createSession(ctx, request)
+	}
+	return createToken(session)
+}
+
+func CreateToken(request *events.GetTokenRequest) *token.Token {
+	ctx := context.Background()
+	session := createSession(ctx, request)
+	return createToken(session)
+}
+
+func refreshExistingSession(ctx context.Context, sessionID pgtype.UUID) *repo.Session {
+	q := repo.New(app.DB)
+
+	session, err := q.GetAliveSession(ctx, sessionID)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil
+	}
+	if err != nil {
+		log.Printf("Failed to query repo for session: %v", err)
+		return fakeSession()
+	}
+
+	params := repo.RefreshSessionParams{SessionID: session.SessionID, SessionDuration: sessionDurationSecs}
+	session, err = q.RefreshSession(ctx, params)
+	if err != nil {
+		log.Printf("Failed to update session: %v", err)
+		return fakeSession()
+	}
+
+	return &session
+}
+
+func createSession(ctx context.Context, request *events.GetTokenRequest) *repo.Session {
+	tx, err := app.DB.Begin(ctx)
+	if err != nil {
+		return fakeSession()
+	}
+	defer tx.Rollback(ctx)
+	q := repo.New(app.DB).WithTx(tx)
+
+    device, err := getOrCreateDevice(ctx, tx, request)
+    if err != nil {
+        return fakeSession()
+    }
+
+	params := repo.CreateSessionParams{SessionID: genUUID(), DeviceID: device.DeviceID, SessionDuration: sessionDurationSecs}
+	session, err := q.CreateSession(ctx, params)
+	if err != nil {
+		return fakeSession()
+	}
+	tx.Commit(ctx)
+
+	return &session
+}
+
+func getOrCreateDevice(ctx context.Context, tx pgx.Tx, request *events.GetTokenRequest) (device repo.Device, err error) {
+	q := repo.New(app.DB).WithTx(tx)
+
+	device, err = q.GetDeviceByExternalDeviceID(ctx, request.DeviceInfo.DeviceID)
+	if err == nil {
+		return
+	}
+	if !errors.Is(err, pgx.ErrNoRows) {
+		log.Printf("Failed to query repo for device: %v", err)
+		return
+	}
+
+	params := repo.CreateDeviceParams{
+		DeviceID:         genUUID(),
+		Source:           request.EventSource,
+		ExternalDeviceID: request.DeviceInfo.DeviceID,
+		// TODO: pass metadata here
+	}
+	device, err = q.CreateDevice(ctx, params)
+	if err != nil {
+		log.Printf("Failed to create device: %v", err)
+	}
+	return
+}
+
+func createToken(session *repo.Session) *token.Token {
+	return &token.Token{
+		SessionID: session.SessionID.Bytes,
+		DeviceID:  session.DeviceID.Bytes,
+		ExpireAt:  session.EndsAt.Time.Unix(),
+	}
+}
+
+func fakeSession() *repo.Session {
+	now := time.Now()
+
+	return &repo.Session{
+		SessionID:  genUUID(),
+		DeviceID:   genUUID(),
+		InsertedAt: toTimestamp(now),
+		UpdatedAt:  toTimestamp(now),
+		EndsAt:     toTimestamp(now.Add(sessionDuration)),
+	}
+}
+
+func genUUID() pgtype.UUID {
+	return pgtype.UUID{
+		Bytes: uuid.New(),
+		Valid: true,
+	}
+}
+
+func toTimestamp(t time.Time) pgtype.Timestamp {
+	return pgtype.Timestamp{Time: t}
+}
